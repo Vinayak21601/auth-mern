a327a39992aed180eb43d1aabb2bfa8c1d8ce83b
Vinayakuponly <Vinayak.devare@uponly.in>
2024-07-28 21:49:13 +0530

    added routes and created pages

--- frontend-auth/src/App.jsx
+++ frontend-auth/src/App.jsx
@@ -1,3 +1,34 @@
+// App.js
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SignUp from "./pages/SignUp";
+import Signin from "./pages/Signin";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/home",
+    element: <Home />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
+  {
+    path: "/sign-in",
+    element: <Signin />,
+  },
+  {
+    path: "/sign-up",
+    element: <SignUp />,
+  },
+]);
+
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world erfdrff!</h1>;
+  return <RouterProvider router={router} />;
 }
