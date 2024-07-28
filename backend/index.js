935a855d8de23639b3e87e42dd07c75fc9e410f8
Vinayakuponly <Vinayak.devare@uponly.in>
2024-07-29 00:45:07 +0530

    connected to db

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,15 @@
 import express from "express";
-
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("connected to mongo db");
+  })
+  .catch((err) => {
+    console.log("Something went wrong");
+  });
 const app = express();
 
 app.listen(3001, () => {
