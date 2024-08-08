5a7ce32e0137872068a01674e869190d84e841b2
Vinayakuponly <Vinayak.devare@uponly.in>
2024-08-08 23:23:23 +0530

    db connected

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRoutes from "./routes/user.routes.js";
 dotenv.config();
 mongoose
   .connect(process.env.MONGO)
@@ -10,8 +11,17 @@ mongoose
   .catch((err) => {
     console.log("Something went wrong");
   });
+
 const app = express();
 
 app.listen(3001, () => {
   console.log("Server listening on port 3000");
 });
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Api is working",
+  });
+});
+
+app.use("/api/user", userRoutes);
