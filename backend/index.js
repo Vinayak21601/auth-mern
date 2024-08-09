fc68085b859b848c0eb912b458670c1dc3db5b47
Vinayakuponly <Vinayak.devare@uponly.in>
2024-08-09 15:41:29 +0530

    created signup controller

--- backend/index.js
+++ backend/index.js
@@ -2,19 +2,24 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from "./routes/user.routes.js";
+import authRoutes from "./routes/auth.routes.js";
 dotenv.config();
+
 mongoose
-  .connect(process.env.MONGO)
+  .connect(
+    "mongodb+srv://vinayakdevare369:vinayakdevare369@auth-mern.dmzdgh5.mongodb.net/auth-mern?retryWrites=true&w=majority&appName=auth-mern"
+  )
   .then(() => {
     console.log("connected to mongo db");
   })
   .catch((err) => {
-    console.log("Something went wrong");
+    console.error("Something went wrong:", err.message);
   });
 
 const app = express();
+app.use(express.json());
 
-app.listen(3001, () => {
+app.listen(3000, () => {
   console.log("Server listening on port 3000");
 });
 
@@ -25,3 +30,4 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
